6aac50757206bd6d572cecc503a0873e69b536b1
Cesar David Lopez Lara <cesar.lopez@qrsof.com>
2024-04-05 17:04:55 -0600

    Init project

--- src/main/scala/com/cesar/netty/NettyClient.scala
+++ src/main/scala/com/cesar/netty/NettyClient.scala
@@ -1,8 +1,8 @@
 package com.cesar.netty
 
-import io.grpc.netty.shaded.io.netty.channel.{ChannelFuture, ChannelInitializer, ChannelOption, EventLoopGroup}
-import io.grpc.netty.shaded.io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.EventLoopGroup
+import io.netty.channel.nio.NioEventLoopGroup
+
 class NettyClient {
   val workerGroup: EventLoopGroup = new NioEventLoopGroup()
-
 }
